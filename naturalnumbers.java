9c1ffd4fe80865bcda8070610414b25ddf54ae79
ashutoshutane <114010934+ashutoshutane@users.noreply.github.com>
2022-09-24 19:19:15 +0530

    Add files via upload

---
+++ naturalnumbers.java
@@ -0,0 +1,18 @@
+
+public class naturalnumbers {
+
+	public static void main(String[] args)
+	{
+		int i;
+		int sum=0;
+		for(i=1;i<=50;i++)	
+		{
+			sum=sum+i;
+			System.out.println(+i);	
+		}
+		    System.out.println("sum of first 50 natural numbers is:"+sum);
+		
+
+	}
+
+}
